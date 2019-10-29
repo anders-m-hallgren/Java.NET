3b97e57bf98b3a4e416259ec75a883152b5681fb
Anders Hallgren <anders@clouds.se>
2019-10-29 22:22:29 +0100

    http2 for dotnet

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
 
 namespace Java.NETcore
 {
@@ -13,11 +11,25 @@ namespace Java.NETcore
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options => {
+                        //HTTP2 over TLS not working on MAC yet https://github.com/dotnet/corefx/issues/33016
+                        /* options.Listen(IPAddress.Any, 5001, option =>
+                        {
+                            option.Protocols = HttpProtocols.Http2;
+                            option.UseHttps("myPrivateServerCert.pfx", "changeit");
+                        }); */
+                        options.ConfigureHttpsDefaults(option => {
+                            option.ServerCertificate=new X509Certificate2("myPrivateServerCert.pfx", "changeit");
+                        });
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- dotnet.Dockerfile
+++ dotnet.Dockerfile
@@ -1,15 +1,20 @@
 FROM mcr.microsoft.com/dotnet/core/sdk:3.0-alpine AS build-env
-RUN apk add nodejs nodejs-npm bash python curl --repository=http://dl-cdn.alpinelinux.org/alpine/edge/main
+RUN apk -U upgrade -a && apk add libuv nodejs nodejs-npm bash python curl
+#--repository=http://dl-cdn.alpinelinux.org/alpine/edge/main
 WORKDIR /app
 # Copy csproj and restore as distinct layers
 COPY *.csproj ./
 RUN dotnet restore
 # Copy everything else and build
 COPY . ./
+RUN rm -rf ClientApp/node_modules
 RUN dotnet publish -c Release -o out
 
 # Build runtime image
 FROM mcr.microsoft.com/dotnet/core/aspnet:3.0-alpine
+EXPOSE 80
+EXPOSE 443
+ENV ASPNETCORE_URLS=https://+;http://+
 WORKDIR /app
 COPY --from=build-env /app/out .
 
