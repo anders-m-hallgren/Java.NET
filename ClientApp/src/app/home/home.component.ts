8447d4978bce464c9291ba4aa32ca9aaa379dac6
Anders Hallgren <anders@clouds.se>
2019-11-10 14:53:57 +0100

    added home java.NET

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public features: Feature[];
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Feature[]>(baseUrl + 'feature').subscribe(result => {
+      this.features = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Feature {
+  name: string;
+  activated: boolean;
+  description: string;
 }
